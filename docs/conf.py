7fd914c6dc0654ceb457cb1590c0cdf4678ff94c
Phil Schaf <flying-sheep@web.de>
2023-07-20 14:33:59 +0200

    no more hacks

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,3 @@
-import sys
 from datetime import datetime
 from importlib.metadata import metadata
 from pathlib import Path
@@ -7,9 +6,6 @@ from sphinx.application import Sphinx
 
 
 HERE = Path(__file__).parent
-
-# necessary for rtd_gh_links’ linkcode support
-sys.path.insert(0, HERE.parent / "src")
 
 # Clean build env
 for file in HERE.glob("scanpydoc.*.rst"):
