e8de52f539f06f8c2e8838ef8b3be01ef5941a0b
Philipp A <flying-sheep@web.de>
2023-08-22 10:56:20 +0200

    improve prettierrc comment

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
-// TODO: switch back to YAML when possible
-//       or switch to mjs once https://github.com/prettier/prettier-vscode/issues/3066 is fixed
+// TODO: switch back to YAML once https://github.com/prettier/prettier/issues/15141 (I assume) is fixed
+//       or at least switch to mjs once https://github.com/prettier/prettier-vscode/issues/3066 is fixed
 
 /** @type {import("prettier").Config} */
 module.exports = {
