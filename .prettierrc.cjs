e65fa38d7e7d16c4b41eb34eb2b0b9dc408593c7
Philipp A <flying-sheep@web.de>
2023-08-22 10:36:23 +0200

    comment

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,6 @@
+// TODO: switch back to YAML when possible
+//       or switch to mjs once https://github.com/prettier/prettier-vscode/issues/3066 is fixed
+
 /** @type {import("prettier").Config} */
 module.exports = {
     plugins: [require.resolve("prettier-plugin-jinja-template")],
