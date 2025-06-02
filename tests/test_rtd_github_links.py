7247ab3803256da7b32f7a3e430350adb518cd0e
Philipp A. <flying-sheep@web.de>
2025-06-02 12:37:29 +0200

    parametrize

--- tests/test_rtd_github_links.py
+++ tests/test_rtd_github_links.py
@@ -150,8 +150,9 @@ def test_as_function(
     assert github_url(f"scanpydoc.{module}.{name}") == f"{prefix}/{obj_path}#L{s}-L{e}"
 
 
-def test_no_line_nos_for_builtin_source() -> None:
-    start, end = _get_linenos(Mapping)
+@pytest.mark.parametrize("cls", [dict, Mapping])
+def test_no_line_nos_for_unavailable_source(cls: type) -> None:
+    start, end = _get_linenos(cls)
     assert start is end is None
 
 
