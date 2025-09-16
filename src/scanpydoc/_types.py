786ccc58ebbf8949e1d025511acd323bfe394961
Philipp A. <flying-sheep@web.de>
2025-09-16 09:40:16 +0200

    mypy

--- src/scanpydoc/_types.py
+++ src/scanpydoc/_types.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 from typing import Generic, TypeVar
 
 
-_GenericAlias: type = type(Generic[TypeVar("_")])
+_GenericAlias: type = type(Generic[TypeVar("_")])  # type: ignore[index]
